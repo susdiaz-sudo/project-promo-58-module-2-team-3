7af642431c9f76ad5f756f1cb5d54805326d1ec5
MCarmen-Camacho <mcarmen.camacho@outlook.es>
2025-10-29 17:08:17 +0100

    Set up JS fot the select inputs in the Fill section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
 
 //-Cosas:
 //--Cambiar la tarjeta cuando la usuaria escribe en los <input>
@@ -15,70 +15,84 @@ console.log('>> Ready :)');
 // QUERY-SELECTOR
 
 //--Hidden--
-const titleDesign = document.querySelector('.js_titleDesign');
-const designSection = document.querySelector('.js_designSection');
+const titleDesign = document.querySelector(".js_titleDesign");
+const designSection = document.querySelector(".js_designSection");
 
-const titleFill = document.querySelector('.js_titleFill');
-const fillSection = document.querySelector('.js_fillSection');
+const titleFill = document.querySelector(".js_titleFill");
+const fillSection = document.querySelector(".js_fillSection");
 
-const titleShare = document.querySelector('.js_titleShare');
-const shareSection = document.querySelector('.js_shareSection');
+const titleShare = document.querySelector(".js_titleShare");
+const shareSection = document.querySelector(".js_shareSection");
 
 // QUERY FILL
-const inputName = document.querySelector('.js_input_name');
-const inputRace = document.querySelector('.js_input_race');
-const inputGender = document.querySelector('.js_input_gender');
-const inputImage = document.querySelector('.js_input_image');
-const inputClass = document.querySelector('.js_input_class');
-const inputAge = document.querySelector('.js_input_age');
-const inputAffiliation = document.querySelector('.js_input_affiliation');
+const inputName = document.querySelector(".js_input_name");
+const inputRace = document.querySelector(".js_input_race");
+const inputGender = document.querySelector(".js_input_gender");
+const inputImage = document.querySelector(".js_input_image");
+const inputClass = document.querySelector(".js_input_class");
+const inputAge = document.querySelector(".js_input_age");
+const inputAffiliation = document.querySelector(".js_input_affiliation");
 
 // QUERY PREVIEW
-const namePreview = document.querySelector('.js_name_preview');
-const imagePreview = document.querySelector('.js_image_preview');
-const racePreview = document.querySelector('.js_race_preview');
-const genderPreview = document.querySelector('.js_gender_preview');
-const classPreview = document.querySelector('.js_class_preview');
-const agePreview = document.querySelector('.js_age_preview');
-const affiliationPreview = document.querySelector('.js_affiliation_preview');
+const namePreview = document.querySelector(".js_name_preview");
+const imagePreview = document.querySelector(".js_image_preview");
+const racePreview = document.querySelector(".js_race_preview");
+const genderPreview = document.querySelector(".js_gender_preview");
+const classPreview = document.querySelector(".js_class_preview");
+const agePreview = document.querySelector(".js_age_preview");
+const affiliationPreview = document.querySelector(".js_affiliation_preview");
+const resetBtn = document.querySelector("js_reset_btn");
+const containerPreview = document.querySelector("js_container_preview");
+const previewUrl = document.querySelector("js_preview_url");
 
 // FUNCIONES
 
-inputName.addEventListener('input', (ev) => {
-    const name = inputName.value;
-    console.log('has escrito', name, 'en el form');
-    namePreview.innerHTML = name;
-})
+inputName.addEventListener("input", (ev) => {
+  const name = inputName.value;
+  namePreview.innerHTML = name;
+});
 
-inputAge.addEventListener('input', (ev) => {
-    const age = inputAge.value;
-    console.log('has escrito', age, 'en el form');
-    agePreview.innerHTML = age;
-})
+inputAge.addEventListener("input", (ev) => {
+  const age = inputAge.value;
+  agePreview.innerHTML = age;
+});
+inputRace.addEventListener("input", (ev) => {
+  const race = inputRace.value;
+  racePreview.innerHTML = race;
+});
+inputGender.addEventListener("input", (ev) => {
+  const gender = inputGender.value;
+  genderPreview.innerHTML = gender;
+});
+inputClass.addEventListener("input", (ev) => {
+  const fillClass = inputClass.value;
+  classPreview.innerHTML = fillClass;
+});
+inputAffiliation.addEventListener("input", (ev) => {
+  const affiliation = inputAffiliation.value;
+  affiliationPreview.innerHTML = affiliation;
+});
 
 // EVENTOS
 
 //--Hidden--
-titleDesign.addEventListener('click', (ev) => {
-    //designSection.classList.toggle('hidden');
-    fillSection.classList.add('hidden');
-    shareSection.classList.add('hidden');
-    designSection.classList.remove('hidden');
+titleDesign.addEventListener("click", (ev) => {
+  //designSection.classList.toggle('hidden');
+  fillSection.classList.add("hidden");
+  shareSection.classList.add("hidden");
+  designSection.classList.remove("hidden");
 });
-titleFill.addEventListener('click', (ev) => {
-    //fillSection.classList.toggle('hidden');
-    designSection.classList.add('hidden');
-    shareSection.classList.add('hidden');
-    fillSection.classList.remove('hidden');
+titleFill.addEventListener("click", (ev) => {
+  //fillSection.classList.toggle('hidden');
+  designSection.classList.add("hidden");
+  shareSection.classList.add("hidden");
+  fillSection.classList.remove("hidden");
 });
-titleShare.addEventListener('click', (ev) => {
-    //shareSection.classList.toggle('hidden');
-    designSection.classList.add('hidden');
-    fillSection.classList.add('hidden');
-    shareSection.classList.remove('hidden');
-
+titleShare.addEventListener("click", (ev) => {
+  //shareSection.classList.toggle('hidden');
+  designSection.classList.add("hidden");
+  fillSection.classList.add("hidden");
+  shareSection.classList.remove("hidden");
 });
 
 // CÓDIGO QUE SE LANZA CUANDO CARGA LA PÁGINA
-
-
